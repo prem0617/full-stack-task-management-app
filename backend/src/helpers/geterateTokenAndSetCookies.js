b5fd9befccaf74201ee338f1884cd0ca9f8a5e43
Prem Patel <138573164+prem0617@users.noreply.github.com>
2025-01-16 14:47:38 +0530

    Update geterateTokenAndSetCookies.js

--- backend/src/helpers/geterateTokenAndSetCookies.js
+++ backend/src/helpers/geterateTokenAndSetCookies.js
@@ -3,24 +3,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.geterateTokenAndSetCookies = void 0;
+exports.generateTokenAndSetCookies = void 0;
 var jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
-var geterateTokenAndSetCookies = function (req, res, userId) {
+var generateTokenAndSetCookies = function (req, res, userId) {
     try {
         // Generate JWT token
         var token = jsonwebtoken_1.default.sign({ userId: userId }, process.env.JWT_SECRET, {
-            expiresIn: "10d",
+            expiresIn: "10d", // Expiry set for 10 days
         });
+
+        // Determine if in production or development environment
+        var isProduction = process.env.NODE_ENV === "production";
+
         res.cookie("foodApp", token, {
             httpOnly: true,
-            secure: false,
-            maxAge: 864000000, // 1 hour in milliseconds
+            secure: isProduction,  // Set to true if in production (HTTPS required)
+            sameSite: isProduction ? 'None' : 'Lax', // Cross-origin cookie handling
+            maxAge: 10 * 24 * 60 * 60 * 1000, // 10 days in milliseconds
         });
+
         return token;
-    }
-    catch (error) {
+    } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Server Error" });
     }
 };
-exports.geterateTokenAndSetCookies = geterateTokenAndSetCookies;
+exports.generateTokenAndSetCookies = generateTokenAndSetCookies;

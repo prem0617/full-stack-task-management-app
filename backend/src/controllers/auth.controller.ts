b6492442726f1a234101d251cea802e8d4bbc5c0
Prem Patel <138573164+prem0617@users.noreply.github.com>
2025-01-16 16:17:26 +0530

    Update auth.controller.ts

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -100,3 +100,19 @@ export const getMe = async (req: Request, res: Response): Promise<void> => {
     res.status(500).json({ error: "Server Error" });
   }
 };
+
+
+export const logout = async (req: Request, res: Response): Promise<void> => {
+  try {
+    res.clearCookie("foodApp", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+    });
+
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.error("Logout error:", error);
+    res.status(500).json({ message: "Server error while logging out" });
+  }
+};
